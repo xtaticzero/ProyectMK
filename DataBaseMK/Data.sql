6dd2cef0516d711a8a32668abee428d21ae40546
Emmanuel <eestradag0300@gmail.com>
2018-11-04 00:36:34 -0600

    sql

--- DataBaseMK/Data.sql
+++ DataBaseMK/Data.sql
@@ -4,5 +4,5 @@ USE STOCK_MARY_KAY_BBDD;
 INSERT CAT_ROL(descripcion) VALUES('ADMINISTRADOR');
 INSERT CAT_ROL(descripcion) VALUES('OPERADOR');
 --USUARIOS DEL APLICATIVO
-INSERT INTO USERS (email, display_name, password, rol_id,fecha)
-VALUES ('user@dominio.com', 'admin', MD5('admin123'), 1,SYSDATE());
+INSERT INTO USERS (email, display_name, password, rol_id)
+VALUES ('user@dominio.com', 'admin', MD5('admin123'), 1);
