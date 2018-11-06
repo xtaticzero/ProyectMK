deabafef8fba53c9f0592e2c692b298f2138eb93
Emmanuel <eestradag0300@gmail.com>
2018-11-06 11:38:16 -0600

    cambios

--- DataBaseMK/Data.sql
+++ DataBaseMK/Data.sql
@@ -7,16 +7,37 @@ INSERT CAT_ROL(descripcion) VALUES('OPERADOR');
 INSERT INTO USERS (email, display_name, password, rol_id)
 VALUES ('user@dominio.com', 'admin', MD5('admin123'), 1);
 
-select USR.user_id,
-            USR.email,
-            USR.display_name,
-            USR.fecha_entrada,
-            USR.fecha_termino,
-            USR.rol_id,
-            ROL.descripcion
-            from USERS USR
-            inner join CAT_ROL on USR.rol_id = CAT_ROL.rol_id
-            where 
-            USR.password = MD5('admin123')
-            and 
-            USR.display_name = 'admin';
+SELECT 
+USR.user_id,
+USR.email,
+USR.display_name,
+USR.fecha_entrada,
+USR.fecha_termino,
+USR.rol_id,
+ROL.descripcion,
+ROL.rol_id,
+ROL.fecha_entrada,
+ROL.fecha_termino
+FROM USERS USR
+INNER JOIN CAT_ROL ROL on USR.rol_id = ROL.rol_id
+WHERE 
+USR.password = MD5('admin123')
+and 
+USR.display_name = 'admin';
+
+SELECT 
+ROL.rol_id,
+ROL.descripcion,
+ROL.fecha_entrada,
+ROL.fecha_termino
+FROM CAT_ROL ROL  WHERE 1=1;
+
+
+ SELECT 
+ROL.rol_id,
+ROL.descripcion,
+ROL.fecha_entrada,
+ROL.fecha_termino
+FROM CAT_ROL ROL  WHERE 1=1 
+ AND 
+ROL.descripcion LIKE 'OPERADOR%';
